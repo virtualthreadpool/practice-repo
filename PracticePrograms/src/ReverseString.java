c773cff6368e97060cb0784e0ced9361f04ec0bc
Nithinkanagala <141832417+Nithinkanagala@users.noreply.github.com>
2026-07-09 17:11:58 +0530

    Delete PracticePrograms directory

--- PracticePrograms/src/ReverseString.java
+++
@@ -1,9 +0,0 @@
-public class ReverseString {
-    String reverse(String str){
-        String rev="";
-        for(int i=str.length()-1;i>=0;i--){
-            rev=rev+str.charAt(i);
-        }
-        return rev;
-    }
-}
--- PracticePrograms/src/SumOfArray.java
+++
@@ -1,9 +0,0 @@
-public class SumOfArray {
-    void sum(int a[]){
-        int sum=0;
-        for(int i=0;i<a.length;i++){
-            sum=sum+a[i];
-        }
-        System.out.print("Sum of Array: "+sum);
-    }
-}
