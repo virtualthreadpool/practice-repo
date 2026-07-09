184bf3883c9b32343b1497fa4239967b50b4ec71
Nithinkanagala <141832417+Nithinkanagala@users.noreply.github.com>
2026-07-09 17:39:04 +0530

    Add files via upload

---
+++ technical-probems/src/main/java/com/aeropelican/StringLogic.java
@@ -0,0 +1,10 @@
+public class StringLogic{
+    void reverse(String str){
+        String rev="";
+        for (int i=str.length()-1;i>=0;i--){
+            rev=rev+str.charAt(i);
+        }
+
+        System.out.println("Reverse = "+rev);
+    }
+}
