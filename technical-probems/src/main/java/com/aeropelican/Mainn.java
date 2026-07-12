3081fa46772a1f31631a692d8cd1a451fba3f243
srijaporalla <srijaporalla@gmail.com>
2026-07-12 22:27:36 +0530

    Add files via upload

---
+++ technical-probems/src/main/java/com/aeropelican/Mainn.java
@@ -0,0 +1,22 @@
+public class Mainn{
+
+    public static void main(String[] args) {
+
+        Operations.reverseString();
+        System.out.println();
+
+        Operations.checkPrime();
+        System.out.println();
+
+        Operations.findPrimeNumbers();
+        System.out.println();
+
+        Operations.findLargest();
+        System.out.println();
+
+        Operations.findSmallest();
+        System.out.println();
+
+        Operations.findSum();
+    }
+}
